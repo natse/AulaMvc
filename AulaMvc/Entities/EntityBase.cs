2ace93bc720b04272d59f07238b9ba918eeabfd6
Francisco <44513794+natse@users.noreply.github.com>
2021-09-04 21:55:28 -0300

    Initial commit

---
+++ AulaMvc/Entities/EntityBase.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities
+{
+    public class EntityBase <T> where T: struct
+    {
+        [Column("Id")]
+        public T Id { get; set; }
+    }
+}
