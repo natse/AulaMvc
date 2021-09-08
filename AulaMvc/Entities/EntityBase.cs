80f745fbe1daea243f02f8a7ee680a89dfe3daf5
franscisco <natse.2013@gmail.com>
2021-09-07 23:15:58 -0300

    Dapper

--- AulaMvc/Entities/EntityBase.cs
+++ AulaMvc/Entities/EntityBase.cs
@@ -1,9 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace Entities
 {
     public class EntityBase <T> where T: struct
     {
+        [Key]
         [Column("Id")]
         public T Id { get; set; }
     }
--- AulaMvc/Entities/ToDo.cs
+++ AulaMvc/Entities/ToDo.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace Entities
 {
-    public class ToDo
+    [Table("Todo")]
+    public class ToDo:EntityBase<int>
     {
-
+        [Required]
+        [Column ("Tarefa")]
+        public string Tarefa { get; set; }
+        
     }
 }
